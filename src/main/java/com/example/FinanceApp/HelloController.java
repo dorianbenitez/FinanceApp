e0b8189a36742067f78fcb172a6f757fe7cbe95c
Dorian Benitez <dorianbenitez97@gmail.com>
2024-08-30 04:10:25 -0500

    Created the get/post calls for local postgres table

--- src/main/java/com/example/FinanceApp/HelloController.java
+++ src/main/java/com/example/FinanceApp/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.FinanceApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.FinanceApp.Objects.User;
+import com.example.FinanceApp.Objects.UserRepository;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.StringJoiner;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloController {
+
+    private final UserRepository userRepository;
+
+    public HelloController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/test")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/getUsers")
+    public Iterable<User> testGetUsers() {
+        Iterable<User> result = this.userRepository.findAll();
+        StringJoiner joiner = new StringJoiner(", ");
+
+        if(result != null && result.iterator().hasNext()) {
+            for (User user : result) {
+                String userString = String.format("User{id=%d, firstName='%s', lastName='%s', dateOfBirth=%s}",
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getDateOfBirth());
+                joiner.add(userString);
+            }
+            System.out.println(joiner.toString());
+            return result;
+        } else {
+            System.out.println("There is no data available.");
+            return result;
+        }
+    }
+
+    @PostMapping("/addUser")
+    public User addOneUser(@RequestBody User user) {
+        return this.userRepository.save(user);
     }
 
 }
---
+++ src/main/java/com/example/FinanceApp/Objects/User.java
@@ -0,0 +1,46 @@
+package com.example.FinanceApp.Objects;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="test_user_table", schema = "public")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    // Hibernate expects entities to have a no-arg constructor,
+    // though it does not necessarily have to be public.
+    private User() {}
+
+    public User(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+}
